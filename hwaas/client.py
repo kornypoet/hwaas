4e37e9d8e8fc96ade0de57348a1194373a7ef53a
Travis Dempsey <dempsey.travis@gmail.com>
2017-05-29 18:57:11 -0500

    catch keyboard interrupts; allow redis_host to be passed in

--- hwaas/client.py
+++ hwaas/client.py
@@ -6,14 +6,17 @@ import time
 from .task import hello_world
 
 class Client():
-    def __init__(self, queue='hwaas', message='hello world'):
-        self.queue = Queue(queue, connection=Redis())
+    def __init__(self, queue='hwaas', redis_host='localhost', message='hello world'):
+        self.queue = Queue(queue, connection=Redis(redis_host))
         self.message = message
         self.logger = logging.getLogger(__name__)
 
     def run(self):
         self.logger.info('Started at %s', time.time())
-        while True:
-            job = self.queue.enqueue_call(func=hello_world, args=(self.message,), result_ttl=30)
-            self.logger.info('Submitted job %s', job.id)
-            time.sleep(1)
+        try:
+            while True:
+                job = self.queue.enqueue_call(func=hello_world, args=(self.message,), result_ttl=30)
+                self.logger.info('Submitted job %s', job.id)
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.logger.warn('Shutting down')
