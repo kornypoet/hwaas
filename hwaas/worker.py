326e8e06af9d33d17d9b93da1a090ecb1d69f15f
Travis Dempsey <dempsey.travis@gmail.com>
2017-05-29 18:56:02 -0500

    allow redis to have a different host passed in

--- hwaas/worker.py
+++ hwaas/worker.py
@@ -1,14 +1,16 @@
 import logging
+from redis import Redis
 import rq
 import time
 
 class Worker():
-    def __init__(self, queue='hwaas', name='name'):
+    def __init__(self, queue='hwaas', redis_host='localhost'):
         self.queue = queue
+        self.redis_host = redis_host
         self.logger = logging.getLogger(__name__)
 
     def run(self):
         self.logger.info('Started at %s', time.time())
-        with rq.Connection():
+        with rq.Connection(Redis(self.redis_host)):
             rq_worker = rq.Worker([self.queue])
             rq_worker.work()
